61c60a9156a3388a3ec8dd7249eb1875f7729d09
Pratik5357 <pkkumbhar1057@gmail.com>
2024-09-19 19:07:10 +0530

    completed Project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Write JavaScript herelet API_KEY = "9155b61e49b1fc170c759b7874d15dc2";
+let API_KEY = "9155b61e49b1fc170c759b7874d15dc2";
 
-let main = document.querySelector(".active");
+let main = document.querySelector(".Info");
 let btn = document.querySelector("#Search");
 
 // btn.addEventListener("click",()=>{
@@ -18,9 +18,13 @@ btn.addEventListener("click", () => {
             if (data.cod === 200) {
                 main.innerHTML = `
                     <h2 class="data-head">${data.name}, ${data.sys.country}</h2>
-                    <p>Temperature: ${data.main.temp}°C</p>
-                    <p>Feels like: ${data.main.feels_like}°C</p>
-                    <p>Weather: ${data.weather[0].description}</p>
+                    <span class="coord"><span class="data lon"><p>${data.coord.lon}</p><p>longitude</p></span><span class="data lat"><p>${data.coord.lat}</p><p>latitude</p></span></span>
+                    <p class="data temp">${data.main.temp} °C</p>
+                    <p class="data">Feels like: ${data.main.feels_like}°C</p>
+                    <p class="data"> ${data.weather[0].description}</p>
+                    <p class="data">humidity : ${data.main.humidity}</p>
+                    <span class="temp2"><span class="data minT"><p>${data.main.temp_min}<i class="fa-solid fa-temperature-low"></i></p></span><span class="data maxT"><p>${data.main.temp_min}<i class="fa-solid fa-temperature-high"></i></p></span></span>
+                    <p class="data wind">Wind : ${data.wind.speed}<i class="fa-solid fa-wind"></i></p>
                 `;
                 
                 main.classList.add("show"); // Apply the show class to trigger transition
